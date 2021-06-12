8a3b814dd3459a12e212a1257d1c0d5feb57e9b0
gauravkeshri0409 <76517225+gauravkeshri0409@users.noreply.github.com>
2021-06-12 13:10:21 +0530

    Add files via upload

---
+++ day-3.py
@@ -0,0 +1,17 @@
+year = int(input('Enter your birth year: '))
+current_year = 2021
+age = current_year - year
+print(f'Your age is: {age}')
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+add = num1 + num2
+subtract = num1 - num2
+divide = num1 / num2
+multiply = num1 * num2
+
+print(f'''The numbers added is {add}'
+The numbers subtracted is {subtract}'
+The numbers multiplied is {multiply}'
+The numbers divided is {divide}''')

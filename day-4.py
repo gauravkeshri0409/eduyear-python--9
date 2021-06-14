58b062ec10f83c31411572ef15d60a1d8006bf50
gauravkeshri0409 <76517225+gauravkeshri0409@users.noreply.github.com>
2021-06-14 13:27:08 +0530

    Add files via upload

---
+++ day-4.py
@@ -0,0 +1,15 @@
+# Count the number of a particular letter in a sentence
+string = "happy new year"
+substring = "y"
+count = string.count(substring)
+print(f'The count is : {count}')
+
+# Print Even Indexes of a Word
+word = input("Enter a word: ")
+modified_word = word[::2]
+print(modified_word)
+
+# Swap Cases of a Word
+name = input("Enter a word: ")
+modified_name = name.swapcase()
+print(modified_name)
